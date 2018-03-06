afc74429be682a1737024bcc14ffc7c8f30195d0
Mightynumnums <deadcookies@gmail.com>
2018-03-06 11:58:36 -0500

    public folder and index and layout files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const app = express();
 const nunjucks = require('nunjucks');
 const morgan = require('morgan');
+const routes = require('./routes');
 
 
 nunjucks.configure('views', {
@@ -9,9 +10,14 @@ nunjucks.configure('views', {
     express: app
 });
 
+app.use('/', routes)
+
 app.engine('html', nunjucks.render);
+
 app.set('view engine', 'html');
+
 nunjucks.configure('views', {noCache: true});
+
 nunjucks.render('index.html', locals, function (err, output) {
     console.log(output);
 });
@@ -27,26 +33,21 @@ var locals = {
 
 const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
-
-
-
-app.use(function (req, res, next) {
-    console.log('Hello')
-    next();
-})
-
-app.get('/', (req, res) =>{
-    res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
-
-app.get('/is-anybody-in-there', (req, res, next) =>{
-    res.send('somebody here');
-})
-
-app.get('/modernism',(req, res, next) =>{
-    res.sendStatus(200);
-})
-
 app.listen(3000, () =>{
     console.log('Hello World!!! It is post 3000.');
 });
+
+
+
+//old routes
+// app.get('/', (req, res) =>{
+//     res.render( 'index', {title: 'Hall of Fame', people: people} );
+// })
+
+// app.get('/is-anybody-in-there', (req, res, next) =>{
+//     res.send('somebody here');
+// })
+
+// app.get('/modernism',(req, res, next) =>{
+//     res.sendStatus(200);
+// })
