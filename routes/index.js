c8f7caf85e23e9ef35a3f4b46e487cc19625315e
Mightynumnums <deadcookies@gmail.com>
2018-03-06 12:14:31 -0500

    css file and stylesheets

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ const router = express.Router();
 // could use one line instead: const router = require('express').Router();
 const tweetBank = require('../tweetBank');
 
+router.use(express.static('./public'));
+
 router.get('/', function (req, res) {
   let tweets = tweetBank.list();
   res.render( 'index', { tweets: tweets } );
 });
+
+
 
 module.exports = router;
